5286477501a4ca532633bd7fc754d01969bb6a8c
Amam Jain <153798500+amamjain10@users.noreply.github.com>
2026-09-16 22:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (72.98%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,14 +1,24 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n= citations.length;
-        Arrays.sort(citations);
+        int buk[] = new int[n+1];
 
         for(int i=0;i<n;i++){
-            if(citations[i] >= n-i){
-                return n-i;
+            int x= citations[i];
+            if(x>=n){
+                buk[n]++;
             }
-        }   
+            else{
+                buk[x]++;
+            }
+        }
+        int count=0;
+        for(int i=n;i>=0;i--){
+            count += buk[i];
+            if(count>=i){
+                return i;
+            }
+        }
         return 0;
-
     }
 }
